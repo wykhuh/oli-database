a27ea187a80a0a81b47c90f421f98bbb3ce4bbd5
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-08-04 08:16:08 -0700

    add script to update video data and oli_id for Oli.csv

---
+++ scripts/my_oli_file.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import envar as envar
+from pathlib import Path
+import hashlib
+
+project_path = Path(__file__).parent.parent
+int_dtype = {'playlist_position': 'Int64', 'price': 'Int64', 'serial_number':'Int64'}
+oli_path = project_path/"raw_data"/"Oli.csv"
+
+def update_video_data():
+    oli_df = pd.read_csv(oli_path, dtype=int_dtype)
+    cols = ['position','video_title','video_id','playlist_id']
+    oli_playlist_df = pd.read_csv(project_path/"raw_data"/"oli_playlist.csv", usecols=cols)
+
+    temp_df = oli_df[pd.isna(oli_df['video_title']) & pd.notna(oli_df['video_id'])]
+    ids = temp_df['video_id'].unique()
+
+    for i, row in oli_playlist_df.iterrows():
+        video_id = row['video_id']
+        if(video_id not in ids):
+            continue
+
+        oli_df.loc[oli_df['video_id'] == video_id, 'playlist_position'] = row['position']
+        oli_df.loc[oli_df['video_id'] == video_id, 'video_title'] = row['video_title']
+        oli_df.loc[oli_df['video_id'] == video_id, 'playlist_id'] = row['playlist_id']
+        oli_df.loc[oli_df['video_id'] == video_id, 'video_type'] = 'sound_sample'
+        oli_df.loc[oli_df['video_id'] == video_id, 'video_provider'] = 'youtube'
+
+    oli_df.to_csv(oli_path, index=False)
+
+
+def add_oili_id():
+    my_oli_df = pd.read_csv(oli_path, dtype=int_dtype)
+    my_oli_df['oli_id'] = ''
+
+    for i, row in my_oli_df.iterrows():
+        if(pd.isna(row['model'])):
+            continue
+
+        temp = row['model'] + row['top_wood'] + row['back_wood']
+        my_oli_df.loc[i, 'oli_id'] = hashlib.md5(temp.encode()).hexdigest()
+
+    my_oli_df.to_csv(oli_path, index=False)
+
+#update_video_data()
+add_oili_id()
