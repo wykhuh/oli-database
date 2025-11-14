3a01bdc3cc5ae2392073091dd71853c37a33fdc9
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2025-11-14 08:43:44 -0800

    refactor model comparison code

--- app/js/models-comparison.js
+++ app/js/models-comparison.js
@@ -1,4 +1,4 @@
-import { getAndParseCSV } from "./dataTable.js";
+import { createTable, getAndParseCSV } from "./dataTable.js";
 import { renderNav } from "./utils.js";
 
 async function init() {
@@ -8,59 +8,20 @@ async function init() {
   if (!loaderEl) return;
   let mainEl = document.querySelector("main");
   if (!mainEl) return;
+  let containerEl = document.querySelector(".table-container");
+  if (!containerEl) return;
 
   loaderEl.className = "loading";
 
   let rawModels = await getAndParseCSV("../data/model_comparison.csv");
 
-  let tableEl = document.querySelector("table");
-  if (!tableEl) return;
-
-  let headerRow = document.createElement("tr");
-  addTH("Feature", headerRow);
-  addTH("L", headerRow);
-  addTH("L1", headerRow);
-  addTH("L2", headerRow);
-  addTH("L3", headerRow);
-  addTH("X1", headerRow);
-  addTH("X2", headerRow);
-  addTH("X3", headerRow);
-  tableEl.appendChild(headerRow);
-
-  rawModels.forEach((row) => {
-    if (row.Feature) {
-      addRow(row, tableEl);
-    }
-  });
+  let config = {
+    displayFields: ["Feature", "L", "L1", "L2", "L3", "X1", "X2", "X3"],
+  };
+  let table = createTable(rawModels, config);
+  containerEl.appendChild(table);
 
   loaderEl.className = "";
-}
-
-function addTH(text, rowEl) {
-  let thEl = document.createElement("th");
-  thEl.innerText = text;
-  rowEl.appendChild(thEl);
-}
-
-function addTD(text, rowEl) {
-  let tdEl = document.createElement("td");
-  tdEl.innerText = text;
-  rowEl.appendChild(tdEl);
-}
-
-function addRow(row, tableEl) {
-  let rowEl = document.createElement("tr");
-
-  addTD(row.Feature, rowEl);
-  addTD(row.L, rowEl);
-  addTD(row.L1, rowEl);
-  addTD(row.L2, rowEl);
-  addTD(row.L3, rowEl);
-  addTD(row.X1, rowEl);
-  addTD(row.X2, rowEl);
-  addTD(row.X3, rowEl);
-
-  tableEl.appendChild(rowEl);
 }
 
 init();
