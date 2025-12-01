1e768791ba6f0c6ad3aa619a17625f9c31356e14
wykhuh <wykhuh@users.noreply.github.com>
2025-12-01 02:15:39 +0000

    deploy: a7bc686e825cbb1b30e19260b129f3c8dcbbace4

--- components/ModelsListPage.js
+++ components/ModelsListPage.js
@@ -1,6 +1,7 @@
 import { html, setupComponent } from "../js/component_utils.js";
 import { renderSortableTable, renderPageIntro } from "../js/dataTable.js";
 import { pluralize } from "../js/utils.js";
+import { config } from "../js/config.js";
 
 let template = html`
   <h1 class="title"></h1>
@@ -24,8 +25,8 @@ export class ModelsListPage extends HTMLElement {
   }
 
   async render() {
-    renderPageIntro(app.store.config.home);
-    renderSortableTable(app.store.models, app.store.config.home);
+    renderPageIntro(config.home);
+    renderSortableTable(app.store.models, config.home);
     this.renderCounter();
   }
 
@@ -36,15 +37,12 @@ export class ModelsListPage extends HTMLElement {
           .filter((m) => m["model"])
           .map((m) => {
             return m["model"].match(/>(.*?)<\/a>/)[1];
-          })
+          }),
       );
       let ids = new Set(models.map((m) => m["Model"]));
       let count = Math.max(links.size, ids.size);
 
-      return `${pluralize(count, "model")}, ${pluralize(
-        models.length,
-        "variation"
-      )} `;
+      return `${pluralize(count, "model")}, ${pluralize(models.length, "variation")} `;
     }
 
     let counterEl = document.getElementById("model-counter");
--- js/store.js
+++ js/store.js
@@ -3,47 +3,6 @@ const store = {
   units: [],
   videos: [],
   tonewoods: [],
-  config: {
-    home: {
-      title: "'Oli Models",
-      displayFields: [
-        "Model",
-        "Tier",
-        "Size",
-        "Top Wood",
-        "Back Wood",
-        "Finish",
-        "Model Notes",
-        "Video Published At",
-      ],
-      link: { textField: "Model", idField: "Oli Id", path: "models" },
-      sortable: true,
-    },
-    modelDetails: {
-      modelFields: [
-        "Model",
-        "Tier",
-        "Size",
-        "Top Wood",
-        "Back Wood",
-        "Finish",
-        "Limited Edition",
-      ],
-      displayFields: ["Model", "Top Wood", "Back Wood", "Model Notes"],
-      unitFields: [
-        "Model",
-        "Top Wood",
-        "Back Wood",
-        "Price",
-        "Serial Number",
-        "Model Notes",
-        "Notes",
-        "Video Published At",
-        "Limited Edition",
-      ],
-      sortable: false,
-    },
-  },
 };
 
 const proxiedStore = new Proxy(store, {});
